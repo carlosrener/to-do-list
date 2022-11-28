277d462ec62cbc75c2e8d9b7318b67adf958cd19
carlosrener <111909053+carlosrener@users.noreply.github.com>
2022-11-27 21:56:53 -0300

    setup after class 4

--- controller/taskController.js
+++ controller/taskController.js
@@ -3,7 +3,7 @@ import Task from "../models/Task.js";
 export const controllerGetAllTask = async (req, res) => {
   try {
     const taskList = await Task.find();
-    return res.render("index", {taskList});
+    return res.render("index", { taskList, task: null, taskDelete: null });
   } catch (error) {
     res.status(500).send({ error: error.message });
   }
@@ -17,10 +17,45 @@ export const controllerCreateTask = async (req, res) => {
   }
 
   try {
-
     await Task.create(task);
     return res.redirect("/");
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+};
 
+export const controllerGetByIdTask = async (req, res) => {
+  try {
+    const taskList = await Task.find();
+
+    if (req.params.method == "update") {
+      const task = await Task.findOne({ _id: req.params.id });
+      res.render("index.ejs", { task, taskDelete: null, taskList });
+    } else {
+      const taskDelete = await Task.findOne({ _id: req.params.id });
+      res.render("index.ejs", { task: null, taskDelete, taskList });
+    }
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+};
+
+export const controllerUpdateTask = async (req, res) => {
+  try {
+    const task = req.body;
+    await Task.updateOne({ _id: req.params.id }, task);
+    res.redirect("/");
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+};
+
+export const controllerDeleteTask = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    await Task.deleteOne({ _id: id });
+    res.redirect("/");
   } catch (error) {
     res.status(500).send({ error: error.message });
   }
--- routes/routes.js
+++ routes/routes.js
@@ -2,11 +2,17 @@ import express from "express";
 import {
   controllerGetAllTask,
   controllerCreateTask,
+  controllerGetByIdTask,
+  controllerUpdateTask,
+  controllerDeleteTask
 } from "../controller/taskController.js";
 
 const routes = express.Router();
 
 routes.get("/", controllerGetAllTask);
 routes.post("/create",controllerCreateTask);
+routes.get("/getbyid/:id/:method", controllerGetByIdTask);
+routes.post("/updateone/:id", controllerUpdateTask);
+routes.get("/deleteone/:id", controllerDeleteTask);
 
 export default routes;
